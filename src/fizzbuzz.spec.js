c82287d1f5347b019db8237b84d27cf5cb8cc9a2
Osber <osberriojalopez@gmail.com>
2023-03-20 19:51:16 -0400

    cosidera todos los numeros que no son divisibles por 3 como no multiplos de 3

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -10,5 +10,8 @@ describe("fizzbuzz", () => {
   it("si el numero es divisible entre 3 es considerado multiplo de 3", () => {
     expect(esMultiplo3(30)).toEqual(true);
   });
+  it("si el numero no es divisible entre 3 no es considerado multiplo de 3", () => {
+    expect(esMultiplo3(1)).toEqual(false);
+  });
 });
 
