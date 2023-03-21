0aa302859997cbc83e90bd7d88893fe7597c7d51
Osber <osberriojalopez@gmail.com>
2023-03-21 00:04:04 -0400

    imprime en pantalla segun el numero ingresado

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,15 @@
 import esMultiplo from "./fizzbuzz.js";
 
 const first = document.querySelector("#primer-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#FizzBuzz-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
+  const cad=esMultiplo(firstNumber);
+  console.log(cad);
 
-  div.innerHTML = "<p>" + esMultiplo(firstNumber) + "</p>";
+  div.innerHTML = "<p>" + cad + "</p>";
 });
